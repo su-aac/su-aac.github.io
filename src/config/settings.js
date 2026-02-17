ba217e296908ad4ceb9258540e29faf04b4ef92e
Morris Huang <r11222015@g.ntu.edu.tw>
2026-02-17 11:19:54 -0500

    update

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
+	title: `Hsiao-Ting Su Personal Website`,
+	description: `Hsiao-Ting is a Taiwanese speech-language pathologist and doctoral candidate at UNC Chapel Hill, sharing AAC knowledge, resources, and clinical experiences to help individuals with communication disabilities access effective communication tools.`,
+	// url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `AAC Action Now`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
