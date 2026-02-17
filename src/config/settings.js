231de0d618926a8bb52a5dacb5a89c4aaec0bcec
Morris Huang <r11222015@g.ntu.edu.tw>
2026-02-17 11:25:04 -0500

    update

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Hsiao-Ting Su Personal Website`,
-	description: `Hsiao-Ting is a Taiwanese speech-language pathologist and doctoral candidate at UNC Chapel Hill, sharing AAC knowledge, resources, and clinical experiences to help individuals with communication disabilities access effective communication tools.`,
+	description: `Hsiao-Ting is a Taiwanese speech-language pathologist and doctoral candidate at UNC Chapel Hill, sharing AAC knowledge, resources, and clinical experiences to support individuals  who cannot rely on speech to be heard and understood in accessing communication and literacy.`,
 	// url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `AAC Action Now`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
